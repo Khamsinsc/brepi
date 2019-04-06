236604b4fce44066675e0e995d105a992bef3fb5
James Tang <senbce@gmail.com>
2019-04-07 00:33:47 +0200

    added switch and nomatch component to router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Beers from './components/Beers';
 import OrdersContainer from './containers/OrdersContainer';
@@ -12,9 +12,12 @@ render(
   <BrowserRouter>
     <Provider store={ store }>
       <App>
-        <Route exact path='/' component={ Home } />
-        <Route path='/beers' component={ Beers } />
-        <Route path='/orders' component={ OrdersContainer } />
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/beers' component={ Beers } />
+          <Route path='/orders' component={ OrdersContainer } />
+          <Route component={ Home } />
+        </Switch>
       </App>
     </Provider>
   </BrowserRouter>
